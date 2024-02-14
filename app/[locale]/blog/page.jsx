f27d3dd81e13f9bf144c1bfde9002bbaaf08858f
Sashcotravel <93523518+Sashcotravel@users.noreply.github.com>
2024-02-14 10:47:19 +0200

    update blog

--- app/[locale]/blog/page.jsx
+++ app/[locale]/blog/page.jsx
@@ -35,8 +35,10 @@ function Blog() {
     const [resTrue, setResTrue] = useState(false)
     const [click1, setClick1] = useState(false)
     const [click2, setClick2] = useState(false)
+    const [click3, setClick3] = useState(false)
     const [cat, setCat] = useState('')
     const [cat2, setCat2] = useState('')
+    const [cat3, setCat3] = useState('')
     const router = useRouter()
     const pathname = usePathname()
     const searchParams = useSearchParams()
@@ -153,6 +155,11 @@ function Blog() {
         setClick2(prevState => !prevState)
     }
 
+    const category3 = (e) => {
+        setCat3(e.target.title)
+        setClick3(prevState => !prevState)
+    }
+
 
 
     return (
@@ -226,47 +233,35 @@ function Blog() {
                         <div className={s.first_item_search}>
                             <div className='hi-dropdown'>
                                 <button className={s.blog_searchbox}
-                                        onClick={() => setClick2(prevState => !prevState)}
-                                >
-                                    <span className='title'>ФІЛЬТРУВАТИ ЗА ДАТИ</span>
-                                    <span className='value'>{cat2 === '' ? "Всі записи" : cat2}</span>
-                                    <span className={`caret ${click2 ? "caret2" : ''}`}></span>
+                                        onClick={() => setClick3(prevState => !prevState)}>
+                                    <span className='title'>ФІЛЬТРУВАТИ ЗА ЗАПИС</span>
+                                    <span className='value'>{cat3 === '' ? "Всі записи" : cat3}</span>
+                                    <span className={`caret ${click3 ? "caret2" : ''}`}></span>
                                 </button>
-                                <ul className={`dropdown-menu ${s.menu} ${click2 ? s.block : ''}`}>
-                                <li title='' className={cat2 === '' ? 'selected' : ''}
-                                        onClick={(e) => category2(e)}>Всі
+                                <ul className={`dropdown-menu ${s.menu} ${click3 ? s.block : ''}`}>
+                                    <li title='' className={cat3 === '' ? 'selected' : ''}
+                                        onClick={(e) => category3(e)}>Останній
                                     </li>
-                                    <li title='2023' className={cat2 === '2023' ? 'selected' : ''}
-                                        onClick={(e) => category2(e)}>2023
+                                    <li title='найбільш популярний'
+                                        className={cat3 === 'найбільш популярний' ? 'selected' : ''}
+                                        onClick={(e) => category3(e)}>найбільш популярний
                                     </li>
-                                    <li title='2022' className={cat2 === '2022' ? 'selected' : ''}
-                                        onClick={(e) => category2(e)}>2022
+                                    <li title='рекомендований' className={cat3 === 'рекомендований' ? 'selected' : ''}
+                                        onClick={(e) => category3(e)}>рекомендований
                                     </li>
                                 </ul>
                             </div>
                         </div>
-                        <div className={s.first_item_search}>
-                            <div className='hi-dropdown'>
-                                <button className={s.blog_searchbox}
-                                        onClick={() => setClick2(prevState => !prevState)}
-                                >
-                                    <span className='title'>ФІЛЬТРУВАТИ ЗА ДАТИ</span>
-                                    <span className='value'>{cat2 === '' ? "Всі записи" : cat2}</span>
-                                    <span className={`caret ${click2 ? "caret2" : ''}`}></span>
-                                </button>
-                                <ul className={`dropdown-menu ${s.menu} ${click2 ? s.block : ''}`}>
-                                <li title='' className={cat2 === '' ? 'selected' : ''}
-                                        onClick={(e) => category2(e)}>Всі
-                                    </li>
-                                    <li title='2023' className={cat2 === '2023' ? 'selected' : ''}
-                                        onClick={(e) => category2(e)}>2023
-                                    </li>
-                                    <li title='2022' className={cat2 === '2022' ? 'selected' : ''}
-                                        onClick={(e) => category2(e)}>2022
-                                    </li>
-                                </ul>
+                        <form>
+                            <div className={s.four_item_search}>
+                                <div className='hi-searchbar'>
+                                    <span className='title'>Пошукова система</span>
+                                    <input type="search" placeholder='Введіть ключове слово' className='value' />
+                                    <Image src='/blog/search.svg' alt='' width={16} height={16} className={s.fa_search} />
+                                </div>
                             </div>
-                        </div>
+                            <div className={s.col_lg_2}></div>
+                        </form>
                     </div>
                 </div>
             </section>
@@ -277,7 +272,6 @@ function Blog() {
                         {
                             articleAll?.map(item => {
                                 // console.log(item)
-
                                 return (
                                     <li key={item.id}>
                                         <Image src={'https://cb.samwash.ua/storage/image/'
@@ -290,15 +284,17 @@ function Blog() {
                                                 item.type === "news"
                                                     ? <span style={{backgroundColor: "#b217aa"}}>Новина</span>
                                                     : <span style={{backgroundColor: "#e2001a"}}>Стаття</span>
-
                                             }
                                         </div>
-                                        <Link href={'/blog/' + item.slug}><h2>{
+                                        <Link href={'/blog/' + item.slug}>
+                                            <h2>{
                                             item?.content[0]?.title.length > 40
                                                 ? item?.content[0]?.title.slice(0, 30) + '...'
                                                 : item?.content[0]?.title
-                                        }</h2></Link>
-                                        <WhiteWithoutHover text={'readMore'} link={'/blog/' + item.slug} clas={'addBut'} />
+                                            }</h2>
+                                        </Link>
+                                        <WhiteWithoutHover text={'readMore'} link={'/blog/' + item.slug}
+                                                           clas={'addBut'} />
                                     </li>
                                 )
                             })
