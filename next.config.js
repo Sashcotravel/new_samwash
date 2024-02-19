0eeecb22c9455c053950af722ce31ee7ee2fde02
Sashcotravel <93523518+Sashcotravel@users.noreply.github.com>
2024-02-19 10:45:13 +0200

    update program

--- next.config.js
+++ next.config.js
@@ -1,4 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withVideos = require('next-videos')
 
-module.exports = nextConfig
+const nextConfig = {
+    webpack(config, options) {
+        config.module.rules.push({
+            test: /\.mp4$/i,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'video',
+                    },
+                },
+            ],
+        });
+        return config;
+    },
+    images: {
+        domains:["cb.samwash.ua"]
+    }
+};
+
+const withNextIntl = require('next-intl/plugin')(
+    './i18n.js'
+);
+
+
+module.exports = withVideos(withNextIntl(nextConfig));
